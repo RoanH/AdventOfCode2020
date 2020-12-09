e833e9a56203aa63343e7fef0ee74eec168bea6f
Roan Hofland <roan.hofland@hotmail.com>
2020-12-09 13:48:15 +0100

    Day 9 Gold Star

--- AdventOfCode/src/me/roan/aoc/day9/Main.java
+++ AdventOfCode/src/me/roan/aoc/day9/Main.java
@@ -15,15 +15,45 @@ public class Main{
 			numbers.add(in.nextLong());
 		}
 	
-		silverStar();
+		long target = silverStar();
+		goldStar(target);
 	}
 	
-	private static void silverStar(){
+	private static void goldStar(long target){
+		int start = 0;
+		int end = 0;
+		long sum = numbers.get(0);
+		
+		while(end + 1 < numbers.size() && start + 1 < numbers.size()){
+			if(sum < target){
+				end++;
+				sum += numbers.get(end);
+			}
+			if(sum > target){
+				sum -= numbers.get(start);
+				start++;
+			}
+			if(sum == target){
+				long min = Long.MAX_VALUE;
+				long max = 0;
+				for(int i = start; i <= end; i++){
+					min = Math.min(min, numbers.get(i));
+					max = Math.max(max, numbers.get(i));
+				}
+				System.out.println("Found: " + start + " ~ " + end + ", min=" + min + ", max=" + max + ", sum=" + (min + max));
+				break;
+			}
+		}
+	}
+	
+	private static long silverStar(){
 		for(int i = 25; i < numbers.size(); i++){
 			if(!sum(i - 25, numbers.get(i))){
 				System.out.println("Num: " + numbers.get(i));
+				return numbers.get(i);
 			}
 		}
+		return -1L;
 	}
 	
 	private static boolean sum(int start, long target){
