461a8f72e04a59e6cb7e867958343be4e623bff3
Roan Hofland <roan.hofland@hotmail.com>
2020-12-19 16:47:52 +0100

    Day 13 Gold Star

--- AdventOfCode/src/me/roan/aoc/day13/Main.java
+++ AdventOfCode/src/me/roan/aoc/day13/Main.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class Main{
 
 	private static List<Integer> busses = new ArrayList<Integer>();
+	private static int[] constraints;
 	private static int arrival;
 	
 	public static void main(String[] args){
@@ -14,13 +15,68 @@ public class Main{
 		arrival = in.nextInt();
 		in.nextLine();
 		String[] parts = in.nextLine().split(",");
-		for(String str : parts){
-			if(!str.equals("x")){
-				busses.add(Integer.parseInt(str));
+		constraints = new int[parts.length];
+		for(int i = 0; i < parts.length; i++){
+			if(!parts[i].equals("x")){
+				int bus = Integer.parseInt(parts[i]);
+				busses.add(bus);
+				constraints[i] = bus;
 			}
 		}
 		
 		silverStar();
+		goldStar();
+	}
+	
+	private static void goldStar(){
+		long n = 1;
+		for(int bus : busses){
+			n *= bus;
+		}
+		
+		long x = 0;
+		for(int i = 0; i < constraints.length; i++){
+			if(constraints[i] == 0){
+				continue;
+			}
+			long a = (constraints[i] - (i % constraints[i]));
+			System.out.println("x = " + a + " (mod " + constraints[i] + ")");
+			
+			long nhat = n / constraints[i];
+			long v = inverse(nhat, constraints[i]);
+			
+			x += a * v * nhat;
+		}
+		
+		System.out.println("time: " + (x % n));
+	}
+	
+	public static final long inverse(long pr, long mod) throws IllegalArgumentException, ArithmeticException{
+		if(mod <= 0 || pr < 0){
+			throw new IllegalArgumentException("Arguments may not be negative");
+		}
+		
+		long px = 1;
+		long t = 0;
+		long r = mod;
+		
+		long q = 0;
+		long tmp;
+		while(r != 0){
+			q = pr / r;
+			
+			tmp = r;
+			r = pr - q * r;
+			pr = tmp;
+			
+			tmp = t;
+			t = px - q * t;
+			px = tmp;
+		}
+		if(pr > 1){
+			throw new ArithmeticException("The given argument is not invertible mod " + mod + " or the modulus is not a prime.");
+		}
+		return px < 0 ? (px + mod) : px;
 	}
 	
 	private static void silverStar(){
