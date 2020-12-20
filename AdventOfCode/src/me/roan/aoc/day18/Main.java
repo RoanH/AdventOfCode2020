db839f9de74bfa2c8e84180c5194efc68dc2b3a8
Roan Hofland <roan.hofland@hotmail.com>
2020-12-20 21:32:36 +0100

    Day 18 Gold Star

--- AdventOfCode/src/me/roan/aoc/day18/Main.java
+++ AdventOfCode/src/me/roan/aoc/day18/Main.java
@@ -16,8 +16,76 @@ public class Main{
 		}
 		
 		silverStar();
+		goldStar();
 	}
 	
+	private static void goldStar(){
+		long sum = 0;
+		for(String problem : problems){
+			System.out.print(problem);
+			System.out.print("=");
+			long ans = solveEq(problem);
+			System.out.println(ans);
+			sum += ans;
+		}
+		System.out.println("Sum: " + sum);
+	}
+	
+	private static long solveEq(String eq){
+		LinkedList<Symbol> data = new LinkedList<Symbol>();
+		for(int i = 0; i < eq.length(); i++){
+			data.add(new Symbol(eq.charAt(i)));
+		}
+		return solveEq(data).value;
+	}
+	
+	private static Symbol solveEq(LinkedList<Symbol> symbols){
+		if(symbols.contains(Symbol.PARENTHESIS_OPEN)){
+			for(int start = 0; start < symbols.size(); start++){
+				if(symbols.get(start).equals(Symbol.PARENTHESIS_OPEN)){
+					int ignore = 0;
+					LinkedList<Symbol> subEq = new LinkedList<Symbol>();
+					
+					Symbol s;
+					while(true){
+						s = symbols.remove(start + 1);
+						if(s.equals(Symbol.PARENTHESIS_OPEN)){
+							ignore++;
+						}else if(s.equals(Symbol.PARENTHESIS_CLOSE)){
+							if(ignore == 0){
+								symbols.set(start, solveEq(subEq));
+								break;
+							}else{
+								ignore--;
+							}
+						}
+						subEq.addLast(s);
+					}
+				}
+			}
+		}
+		
+		for(int i = 0; i < symbols.size(); i++){
+			if(symbols.get(i).equals(Symbol.ADD)){
+				symbols.set(i - 1, new Symbol(symbols.get(i - 1).value + symbols.get(i + 1).value));
+				symbols.remove(i);
+				symbols.remove(i);
+				i--;
+			}
+		}
+		
+		for(int i = 0; i < symbols.size(); i++){
+			if(symbols.get(i).equals(Symbol.MUL)){
+				symbols.set(i - 1, new Symbol(symbols.get(i - 1).value * symbols.get(i + 1).value));
+				symbols.remove(i);
+				symbols.remove(i);
+				i--;
+			}
+		}
+
+		return symbols.get(0);
+	}
+		
 	private static void silverStar(){
 		long result = 0;
 		for(String problem : problems){
@@ -87,5 +155,35 @@ public class Main{
 			}
 		}
 		throw new RuntimeException("Malformed input");
+	}
+	
+	private static class Symbol{
+		private static final Symbol PARENTHESIS_OPEN = new Symbol('(');
+		private static final Symbol PARENTHESIS_CLOSE = new Symbol(')');
+		private static final Symbol MUL = new Symbol('*');
+		private static final Symbol ADD = new Symbol('+');
+		private char sym;
+		private long value;
+		
+		private Symbol(long value){
+			this.value = value;
+		}
+		
+		private Symbol(char ch){
+			sym = ch;
+			if('0' <= ch && ch <= '9'){
+				value = ch - '0';
+			}
+		}
+		
+		@Override
+		public boolean equals(Object other){
+			return other instanceof Symbol && ((Symbol)other).sym == sym;
+		}
+		
+		@Override
+		public String toString(){
+			return "Symbol[sym=" + sym + ",value=" + value + "]";
+		}
 	}
 }
